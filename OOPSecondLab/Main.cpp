fa4ecb6f359f31e0cabdf335fad9264e05bd433c
javantlang <javantlang@gmail.com>
2020-10-17 12:10:21 +0500

    Создание  Point3D, который наследуется от  Point.

--- OOPSecondLab/Main.cpp
+++ OOPSecondLab/Main.cpp
@@ -11,7 +11,7 @@ public:
         y = 0;
     }
     Point(int x, int y) {
-        cout << "Point(int x, int y)\n";
+        printf("Point3D(int %d, int %d)\n", x, y);
         this->x = x;
         this->y = y;
     }
@@ -36,24 +36,63 @@ void Point::decrease() {
     --y;
 }
 
+class Point3D : public Point {
+private:
+    int z;
+public:
+    Point3D() {
+        cout << "Point3D()\n";
+        z = 0;
+    }
+    Point3D(int x, int y, int z) {
+        printf("Point3D(int %d, int %d, int %d)\n", x, y, z);
+        this->x = x;
+        this->y = y;
+        this->z = z;
+    }
+    Point3D(Point3D& p) {
+        cout << "Point3D(Point3D& p)\n";
+        x = p.x;
+        y = p.y;
+        z = p.z;
+    }
+    ~Point3D() {
+        printf("~Point3D(%d, %d, %d)\n", x, y, z);
+    }
+};
+
 int main()
 {
+    setlocale(0, "");
     cout << "Word start\n";
     {
+        cout << "Создание объектов Point в стеке:\n";
         Point staticP1;
         Point staticP2(7, 7);
         Point staticP3(staticP2);
 
         staticP1.increase();
         staticP3.decrease();
+        cout << "Удаление объектов Point в стеке:\n";
     }
 
+    cout << "Создание объектов Point в куче:\n";
     Point* dynamicP1 = new Point();
     Point* dynamicP2 = new Point(9, 9);
     Point* dynamicP3 = new Point(*dynamicP2);
 
+    dynamicP2->decrease();
+    cout << "Удаление объектов Point в куче:\n";
     delete dynamicP1;
     delete dynamicP2;
     delete dynamicP3;
+
+    {
+        cout << "Создание объектов Point3D в стеке:\n";
+        Point3D staticP3D1;
+        Point3D staticP3D2(7, 7, 7);
+        Point3D staticP3D3(staticP3D2);
+        cout << "Удаление объектов Point3D в стеке:\n";
+    }
 }
 
