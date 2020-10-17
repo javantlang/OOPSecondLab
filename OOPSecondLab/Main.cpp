e60e7c4b10156d85f8c50344c42581b703f87de9
javantlang <javantlang@gmail.com>
2020-10-17 10:46:50 +0500

    Create constructor and destructor

--- OOPSecondLab/Main.cpp
+++ OOPSecondLab/Main.cpp
@@ -3,7 +3,25 @@
 using namespace std;
 
 class Point {
-
+public:
+    int x, y;
+    Point() {
+        cout << "Point()\n";
+        x = 0;
+        y = 0;
+    }
+    Point(int x, int y) {
+        cout << "Point(int x, int y)\n";
+        this->x = x;
+        this->y = y;
+    }
+    Point(Point& p) {
+        x = p.x;
+        y = p.y;
+    }
+    ~Point() {
+        cout << "~Point()\n";
+    }
 };
 
 int main()
