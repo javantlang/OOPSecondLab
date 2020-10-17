bbf684d2e337c25738840f0a5f59e3c1fb295ec2
javantlang <javantlang@gmail.com>
2020-10-17 12:53:49 +0500

    Обновление конструктора Point3d(x, y, z). Переопределение increase() & decrease()

--- OOPSecondLab/Main.cpp
+++ OOPSecondLab/Main.cpp
@@ -44,10 +44,8 @@ public:
         cout << "Point3D()\n";
         z = 0;
     }
-    Point3D(int x, int y, int z) {
+    Point3D(int x, int y, int z) : Point(x, y) {
         printf("Point3D(int %d, int %d, int %d)\n", x, y, z);
-        this->x = x;
-        this->y = y;
         this->z = z;
     }
     Point3D(Point3D& p) {
@@ -60,15 +58,17 @@ public:
         printf("~Point3D(%d, %d, %d)\n", x, y, z);
     }
 
-    void increase3D() {
-        this->increase();
+    void increase() {
+        ++x;
+        ++y;
         ++z;
     }
-    void decrease3D();
+    void decrease();
 };
 
-void Point3D::decrease3D() {
-    this->decrease();
+void Point3D::decrease() {
+    --x;
+    --y;
     --z;
 }
 
@@ -104,8 +104,8 @@ int main()
         Point3D staticP3D2(7, 7, 7);
         Point3D staticP3D3(staticP3D2);
 
-        staticP3D1.increase3D();
-        staticP3D3.decrease3D();
+        staticP3D1.increase();
+        staticP3D3.decrease();
         cout << "\nУдаление объектов Point3D в стеке:\n";
     }
 
@@ -114,10 +114,17 @@ int main()
     Point3D* dynamicP3D2 = new Point3D(9, 9, 9);
     Point3D* dynamicP3D3 = new Point3D(*dynamicP3D2);
 
-    dynamicP3D2->decrease3D();
+    dynamicP3D2->decrease();
     cout << "\nУдаление объектов Point3D в куче:\n";
     delete dynamicP3D1;
     delete dynamicP3D2;
     delete dynamicP3D3;
+
+    cout << "\nСоздание объекта Point3D и помещение его адреса в переменную-указатель класса Point\n";
+    Point* multiPoint = new Point3D(2, 2, 2);
+    
+    multiPoint->increase();
+    cout << "\nУдаление объекта Point3D, помещенённого его в переменную-указатель класса Point\n";
+    delete multiPoint;
 }
 
